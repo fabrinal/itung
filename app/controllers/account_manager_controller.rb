79666b30611c2e567a6cabbc369b9e66d9de9df0
Fabian Rinaldi <code.optica@gmail.com>
2015-01-28 20:38:11 +0800

    remove negative signs from expense and edit view of account summary

--- app/controllers/account_manager_controller.rb
+++ app/controllers/account_manager_controller.rb
@@ -11,14 +11,14 @@ class AccountManagerController < ApplicationController
         if account.account_type == "income"
           @total_income += account.value
         else
-          @total_expense += (account.value * -1)
+          @total_expense += (account.value )
         end
     }
   end
 
   def accounts_summary
     summarize_account
-    @balance = @total_income + @total_expense
+    @balance = @total_income - @total_expense
     @expense_accounts =  Account.where(:account_type => "expense")
     @income_accounts =  Account.where(:account_type => "income")
     respond_to do |format|
