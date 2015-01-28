5f3239d94b79b2fafac278da6bc08c91fdf75405
Fabian Rinaldi <code.optica@gmail.com>
2015-01-28 20:16:01 +0800

    Create views, add partial view, refactor using partial,add summary

--- app/controllers/account_manager_controller.rb
+++ app/controllers/account_manager_controller.rb
@@ -3,6 +3,7 @@ class AccountManagerController < ApplicationController
   def initialize
     @total_expense = 0
     @total_income = 0
+    @expense_accounts = ""
   end
 
   def summarize_account
@@ -18,6 +19,8 @@ class AccountManagerController < ApplicationController
   def accounts_summary
     summarize_account
     @balance = @total_income + @total_expense
+    @expense_accounts =  Account.where(:account_type => "expense")
+    @income_accounts =  Account.where(:account_type => "income")
     respond_to do |format|
       format.html{
         render :layout => 'summary'
